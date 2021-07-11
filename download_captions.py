71048b42b1252f2df6f85eab7aad3d6a328a177c
TravisWongX <travis.x.wong@gmail.com>
2021-07-11 18:14:25 +0800

    update

--- download_captions.py
+++ download_captions.py
@@ -2,10 +2,6 @@ import instaloader
 
 L = instaloader.Instaloader()
 
-# acc = 'lauren.jumps'
-
-# L.download_profile(acc, profile_pic_only=True)
-
 L.load_session_from_file('x20920')
 posts = instaloader.Profile.from_username(
     L.context, "lauren.jumps").get_posts()
