fc34d889223cf4827b2f76175a157ee792077813
TravisWongX <travis.x.wong@gmail.com>
2021-07-11 19:01:19 +0800

    update

--- download_captions.py
+++ download_captions.py
@@ -1,8 +1,8 @@
 import instaloader
 
 L = instaloader.Instaloader()
-
-L.load_session_from_file('x20920')
+L.login(user='', passwd='')  # 填入你的ins用户名和密码
+# L.load_session_from_file(username='')
 posts = instaloader.Profile.from_username(
     L.context, "lauren.jumps").get_posts()
 
